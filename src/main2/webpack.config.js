debbbff044fce4e1529294d89a05aa767a6b99b1
Fajran Iman Rusadi <fajran@gmail.com>
2019-05-02 00:54:02 +0200

    Add static pages

--- src/main2/webpack.config.js
+++ src/main2/webpack.config.js
@@ -3,10 +3,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+function staticPage(name) {
+    return new HtmlWebpackPlugin({
+        filename: name + '/index.html',
+        template: 'src/' + name + '.hbs',
+        chunks: ['main', 'static'],
+    })
+}
+
 module.exports = {
     entry: {
         main: './src/main.js',
         tabulasi: './src/tabulasi/tabulasi.ts',
+        static: './src/static.js',
     },
     output: {
         filename: '[name].[contenthash].js',
@@ -22,6 +31,13 @@ module.exports = {
             template: 'src/index.hbs',
             chunks: ['main', 'tabulasi'],
         }),
+        staticPage('disclaimer'),
+        staticPage('kontak'),
+        staticPage('faq'),
+        staticPage('privasi'),
+        staticPage('jenis-peran-pengunjung'),
+        staticPage('tentang'),
+        staticPage('visualisasi'),
     ],
     module: {
         rules: [
