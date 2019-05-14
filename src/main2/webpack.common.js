eb34135636267941c248a75bbb722c1847f8e920
Akeda Bagus <akeda.bagus@automattic.com>
2019-05-14 22:17:44 +0700

    Add autoprefixer.

--- src/main2/webpack.common.js
+++ src/main2/webpack.common.js
@@ -71,7 +71,12 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
             },
             {
                 test: /\.tsx?$/,
