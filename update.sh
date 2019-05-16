8111632d37d15b0e61bc49c8fb7be69619fcef73
Fajran Iman Rusadi <fajran@gmail.com>
2019-05-16 03:39:18 +0200

    add old page as index2.html

--- update.sh
+++ update.sh
@@ -15,6 +15,6 @@ sudo mkdir -p _public
 sudo rm -rf _public/*
 sudo chown -R 1000:1000 _public
 
-rsync -avH --progress --delete-excluded src/tabulasi/tabulasi/index.html _public/index2.html
 rsync -avH --progress --delete-excluded src/main2/dist/ _public/
+rsync -avH --progress src/tabulasi/tabulasi/index.html _public/index2.html
 
