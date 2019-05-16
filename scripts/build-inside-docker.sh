863ece5870e2814894c3064d385afbe473f5183a
Fajran Iman Rusadi <fajran@gmail.com>
2019-05-16 03:39:18 +0200

    Sorting based on inner text and query path issue #13

--- scripts/build-inside-docker.sh
+++ scripts/build-inside-docker.sh
@@ -19,6 +19,6 @@ popd
 # copy for public
 
 mkdir -p /kp/_public/
-rsync -avH --delete-excluded /kp/src/tabulasi/tabulasi/index.html /kp/_public/index2.html
 rsync -avH --delete-excluded /kp/src/main2/dist/ /kp/_public/
+rsync -avH /kp/src/tabulasi/tabulasi/index.html /kp/_public/index2.html
 
